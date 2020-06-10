235e981b1a9c63ee096d0091d3d7f6b3d49d90fb
Sean Cone <seandoesitbetter@gmail.com>
2020-06-09 22:28:32 -0700

    drag and drop fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -191,7 +191,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
       event.preventDefault();
-      
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -212,8 +212,16 @@ var dropTaskHandler = function(event) {
       else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
       }
+      dropZoneEl.removeAttribute("style");
       dropZoneEl.appendChild(draggableElement);  
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+    taskListEl.removeAttribute("style");
+    }
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
@@ -224,3 +232,5 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
